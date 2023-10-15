e509a4539854552ea3349fcaf61592a26de9de02
mohamed98 <mohamedatta447@gmail.com>
2023-10-15 03:10:43 +0300

    Adding home page and work on the header

--- app/bedrock/web/app/themes/atta-theme/resources/views/sections/header.blade.php
+++ app/bedrock/web/app/themes/atta-theme/resources/views/sections/header.blade.php
@@ -2,7 +2,7 @@
   <a class="brand" href="{{ home_url('/') }}">
     {!! $siteName !!}
   </a>
-
+<span class="flex">Hi</span>
   @if (has_nav_menu('primary_navigation'))
     <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
       {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
